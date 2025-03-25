b928dbe50f78fc4889aa656c9d10d5d11cbdaa54
huxichun <huxichun163@163.com>
2025-03-25 14:43:44 +0800

    init

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,53 +7,21 @@
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
+  TouchableOpacity,
+  Text,
+  Alert,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import { sendMessage, watchEvents } from 'react-native-watch-connectivity';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -61,16 +30,51 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  /*
-   * To keep the template simple and small we're adding padding to prevent view
-   * from rendering under the System UI.
-   * For bigger apps the reccomendation is to use `react-native-safe-area-context`:
-   * https://github.com/AppAndFlow/react-native-safe-area-context
-   *
-   * You can read more about it here:
-   * https://github.com/react-native-community/discussions-and-proposals/discussions/827
-   */
-  const safePadding = '5%';
+  React.useEffect(() => {
+    const unsubscribe = watchEvents.on('message', (e) => {
+      console.log(e);
+      Alert.alert('Message Received', `Action: ${e.action} \nData: ${JSON.stringify(e.data)}`);
+      switch (e.action) {
+        case 'login':
+          handleLogin();
+          break;
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  function handleLogin() {
+    sendMessage({
+      action: 'logged',
+      data: {
+        user: {
+          userId: '123',
+        },
+        emergencyContacts: ['17261169754'],
+      },
+    });
+  }
+
+  function handleLogout() {
+    sendMessage({
+      action: 'logout',
+    });
+  }
+
+  function triggerSOS() {
+    sendMessage({
+      action: 'trigger_sos',
+      data: 140.0,
+    });
+  }
+
+  function getHistory() {
+    sendMessage({
+      action: 'get_history',
+    });
+  }
 
   return (
     <View style={backgroundStyle}>
@@ -80,52 +84,72 @@ function App(): React.JSX.Element {
       />
       <ScrollView
         style={backgroundStyle}>
-        <View style={{paddingRight: safePadding}}>
-          <Header/>
-        </View>
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-            paddingHorizontal: safePadding,
-            paddingBottom: safePadding,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: 100,
+        }}>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              width: 150,
+              backgroundColor: 'blue',
+              borderRadius: 10,
+              marginTop: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={getHistory}
+          >
+            <Text>Get History</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              width: 150,
+              backgroundColor: 'green',
+              borderRadius: 10,
+              marginTop: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={handleLogin}
+          >
+            <Text>Send Logged Msg</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              width: 150,
+              backgroundColor: 'yellow',
+              borderRadius: 10,
+              marginTop: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={handleLogout}
+          >
+            <Text>Send Logout Msg</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              width: 150,
+              backgroundColor: 'red',
+              borderRadius: 10,
+              marginTop: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+            onPress={triggerSOS}
+          >
+            <Text>Trigger SOS</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
